01b655660add7092f03d3593052ce5924e1ea9f1
zenglzh <zenglzh@gmail.com>
2013-07-11 09:44:35 +0800

    Update IObjectAdapter.java

--- src/main/java/org/zlzh/util/IObjectAdapter.java
+++ src/main/java/org/zlzh/util/IObjectAdapter.java
@@ -1,30 +1,27 @@
 /*
  * @(#)IObjectAdapter.java  2013-6-26	
  *
- * Copyright  2010 Join-Cheer Corporation Copyright (c) All rights reserved.
- * BEIJING JOIN-CHEER SOFTWARE CO.,LTD
  */
 
 package org.zlzh.util;
 
 /**
- * <p>Title:对象适配接口</p>
+ * <p>Title:锟斤拷锟斤拷锟斤拷锟斤拷锟接匡拷</p>
  * <p>Description:
- * E:源数据对象的对象类型
- * F:适配以后的数据对象类型
+ * E:源锟斤拷锟捷讹拷锟斤拷锟侥讹拷锟斤拷锟斤拷锟斤拷
+ * F:锟斤拷锟斤拷锟皆猴拷锟斤拷锟斤拷锟捷讹拷锟斤拷锟斤拷锟斤拷
  * </p>
  * @author:  zenglizhi
  * @time:    2013-6-26
  * @version:  v1.0
- * @since:    JQR5.0
  */
 public interface IObjectAdapter<E,F> {
 	
 
 	/**
-	 * 将对象 e 适配成对象 F 
-	 * @param e 源对象
-	 * @return F 转换以后的对象
+	 * 锟斤拷锟斤拷锟斤拷 e 锟斤拷锟斤拷锟缴讹拷锟斤拷 F 
+	 * @param e 源锟斤拷锟斤拷
+	 * @return F 转锟斤拷锟皆猴拷锟侥讹拷锟斤拷
 	 */
 	F adapter(E e);
 	
